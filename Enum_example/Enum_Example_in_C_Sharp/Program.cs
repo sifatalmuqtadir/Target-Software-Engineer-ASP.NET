d15a6e56c61adff87724bacdd2bf17e08c5bed9a
sifatalmuqtadir <23-50146-1@student.aiub.edu>
2026-08-14 21:39:12 +0600

    Delete Enum_example directory

--- Enum_example/Enum_Example_in_C_Sharp/Program.cs
+++
@@ -1,9 +0,0 @@
-﻿using Enum_Example_in_C_Sharp;
-
-OrderStatus status = OrderStatus.Delivering;
-
-Console.WriteLine("Order Status: " + status);
-
-Console.WriteLine("Numeric Value: " + (int)status);
-
-Console.ReadLine();
